6de9d92af206bcd256e1650a8a21f99a9fe9d9ce
daorejuela1 <daorejuela1@outlook.com>
2021-03-18 08:04:52 +0000

    modified routes to use resources instead of manual naming

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,11 +1,14 @@
 class Follow < ApplicationRecord
   validates :follower, presence: true
   validates :following, presence: true
+  validate :following_otheruser
 
-  belongs_to :follower, foreign_key: 'user_id', class_name: 'User'
-  belongs_to :following, foreign_key: 'following_id', class_name: 'User'
+  belongs_to :follower, foreign_key: 'user_id', class_name: 'User', primary_key: :username
+  belongs_to :following, foreign_key: 'following_id', class_name: 'User', primary_key: :username
 
-  def follow_validuser
-
+  def following_otheruser
+    if (follower == following)
+      errors.add(:follow, 'You can not follow yourself')
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,13 +15,13 @@ class User < ApplicationRecord
 
   has_many :tweets, dependent: :delete_all
 
-  has_many :follows, dependent: :destroy
+  has_many :follows, dependent: :destroy, primary_key: :username
 
-  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
-  has_many :followers, through: :follower_relationships, source: :follower
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow', primary_key: :username
+  has_many :followers, through: :follower_relationships, source: :follower, primary_key: :username
 
-  has_many :following_relationships, foreign_key: :user_id, class_name: 'Follow'
-  has_many :following, through: :following_relationships, source: :following
+  has_many :following_relationships, foreign_key: :user_id, class_name: 'Follow', primary_key: :username
+  has_many :following, through: :following_relationships, source: :following, primary_key: :username
 
   private
   def set_default_image
